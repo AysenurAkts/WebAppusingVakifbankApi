8586e61247cd3df159c7a6fe7cc25d6eb97752ae
AysenurAkts <81049064+AysenurAkts@users.noreply.github.com>
2021-11-10 16:36:00 +0300

    Update ConstVariables.cs

--- GoldPriceCrudAspNetCoreWebAPI/GoldPriceCrudAspNetCoreWebAPI/Constants/ConstVariables.cs
+++ GoldPriceCrudAspNetCoreWebAPI/GoldPriceCrudAspNetCoreWebAPI/Constants/ConstVariables.cs
@@ -10,8 +10,8 @@ namespace GoldPriceCrudAspNetCoreWebAPI.Constants
     public class ConstVariables 
     {
         public const string grant_type = "client_credentials";
-        public const string client_id = "l7xxc086bdb62c6844068d3b2287d6309f4c";
-        public const string client_secret = "5581ee3687c6487bb2ec964c4315f441";
+        public const string client_id = "**";
+        public const string client_secret = "**";
         public const string scope = "public";
     }
 }
